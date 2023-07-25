2ab93bbc4851a3c4838783aad7947b5bc2630b3f
pritsam <pritesh3458@gmail.com>
2023-07-25 12:53:42 +0200

    chore: form control in NoteForm

--- src/Components/NoteForm.tsx
+++ src/Components/NoteForm.tsx
@@ -1,23 +1,49 @@
-import { Form, Stack, Row, Col } from "react-bootstrap";
-import CreatableReactSelect from 'react-select/creatable';
+import { FormEvent, useRef } from "react";
+import { Form, Stack, Row, Col, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import CreatableReactSelect from "react-select/creatable";
+
 export const NoteForm = () => {
+    const titleRef= useRef(null)
+    const markdownRef=useRef(null)
+
+    const handleSubmit=(e:FormEvent)=>{
+        e.preventDefault();
+        
+    }
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Stack gap={4}>
         <Row>
           <Col>
             <Form.Group controlId="title">
               <Form.Label>Title</Form.Label>
-              <Form.Control required />
+              <Form.Control required ref={titleRef}/>
             </Form.Group>
           </Col>
           <Col>
             <Form.Group controlId="tags">
               <Form.Label>Tags</Form.Label>
-              <CreatableReactSelect isMulti/>
+              <CreatableReactSelect isMulti />
             </Form.Group>
           </Col>
         </Row>
+        <Row>
+          <Form.Group controlId="markdown">
+            <Form.Label>Write Text</Form.Label>
+            <Form.Control required as="textarea" rows={10} ref={markdownRef}></Form.Control>
+          </Form.Group>
+        </Row>
+        <Stack direction="horizontal" gap={2} className="justify-content-end">
+          <Button type="submit" variant="primary">
+            Save
+          </Button>
+          <Link to="..">
+            <Button type="button" variant="outline-secondary">
+              Cancel
+            </Button>
+          </Link>
+        </Stack>
       </Stack>
     </Form>
   );
